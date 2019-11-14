b984f994dc825f79dc3dd062554b6e550388c06b
Alan Meekins <alanm@roshub.io>
2019-11-14 03:33:04 -0800

    live-funding status

--- src/endpoints/funding-status.js
+++ src/endpoints/funding-status.js
@@ -7,23 +7,70 @@ const DefaultConfig = require('../default-config')
 
 let stripe = Stripe(process.env.STRIPE_KEY)
 
+let cacheTotalAmount = undefined
+let lastUpdate = moment()
 
+
+const crawlOrderStatus = async () => {
+
+  let totalAmount = 0
+  let totalOrders = 0
+  let last_order = null
+  let has_more = true
+
+  while(has_more){
+    const orders = await stripe.orders.list({
+      created: {
+        gt: 1573729756
+      },
+      limit: 100,
+      starting_after: last_order || undefined
+    })
+
+    has_more = orders.has_more
+
+    if(has_more){
+      last_order = orders.data[ orders.data.length - 1 ].id
+    }
+
+    orders.data.map((val, idx, arr)=>{
+      if(val.status == 'paid' || val.status == 'fulfilled' || val.status == 'refunded')
+      totalAmount += val.amount
+    })
+
+    totalOrders++
+  }
+
+  debug('total raised -', totalAmount, 'on', totalOrders, 'orders')
+
+  return totalAmount
+}
+
+crawlOrderStatus().then(debug)
 
 module.exports.funding_status = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false; 
 
-      callback(null,{
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-      },
-      body: JSON.stringify({
-        funding: 1000,
-        goal: 49900,
-        accepting: true,
-        start: moment().startOf('isoWeek').toDate(),
-        end: moment().endOf('isoWeek').toDate()
-      })
-    });
+  const deltaTime = lastUpdate.diff(moment())
+
+  if(cacheTotalAmount === undefined || deltaTime > 600){
+    cacheTotalAmount = await crawlOrderStatus()
+    lastUpdate = moment()
+  }
+  
+
+  callback(null,{
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({
+      funding: cacheTotalAmount,
+      goal: 49900,
+      accepting: true,
+      start: moment().startOf('isoWeek').toDate(),
+      end: moment().endOf('isoWeek').toDate()
+    })
+  });
 }
