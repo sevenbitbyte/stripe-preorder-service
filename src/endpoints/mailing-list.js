deef24a42fd2791e69c192878e23f631350fcf59
clovis1122 <clovis-@live.com>
2019-11-24 12:08:30 -0400

    wraps mailing list in a try/catch

--- src/endpoints/mailing-list.js
+++ src/endpoints/mailing-list.js
@@ -9,65 +9,74 @@ let sendy = new Sendy(process.env.SENDY_URL, process.env.SENDY_KEY)
 const SendyList = process.env.SENDY_LIST
 
 const schema = Joi.object().keys({
-  email: Joi.string().email().required(),
-});
+  email: Joi.string()
+    .email()
+    .required(),
+})
 
-const SendySubscribe = async (email)=>{
-  return new Promise((resolve, reject)=>{
+const SendySubscribe = async email => {
+  return new Promise((resolve, reject) => {
+    sendy.subscribe(
+      {
+        email: email,
+        list_id: SendyList,
+      },
+      (err, result) => {
+        if (err) {
+          debug('subscribe error', err)
+          return reject(err)
+        }
 
-    sendy.subscribe({
-      email: email,
-      list_id: SendyList
-    }, (err, result)=>{
-
-      if(err){
-        debug('subscribe error', err)
-        return reject(err)
+        return resolve(result)
       }
-
-      return resolve(result)
-
-    })
+    )
   })
-  
 }
 
 module.exports.mailing_list = async (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false; 
+  context.callbackWaitsForEmptyEventLoop = false
 
   console.log('has account')
 
   console.log(event.body)
 
-  const valid = Joi.attempt(
-    JSON.parse(event.body),
-    schema
-  )
+  try {
+    const valid = Joi.attempt(JSON.parse(event.body), schema)
 
-  
-  let subscribeStatus = false
-  try{
-    subscribeStatus = await SendySubscribe(valid.email)
-  }
-  catch(err){
-    if(subscribeStatus instanceof Error){
-      subscribeStatus = err.message
-    } else {
-      subscribeStatus = err
+    let subscribeStatus = false
+    try {
+      subscribeStatus = await SendySubscribe(valid.email)
+    } catch (err) {
+      if (subscribeStatus instanceof Error) {
+        subscribeStatus = err.message
+      } else {
+        subscribeStatus = err
+      }
     }
-  }
 
-  debug(subscribeStatus)
+    debug(subscribeStatus)
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({
-      joined: subscribeStatus === true,
-      error: (subscribeStatus !== true ? subscribeStatus : null)
-    })
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        joined: subscribeStatus === true,
+        error: subscribeStatus !== true ? subscribeStatus : null,
+      }),
+    }
+  } catch (e) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        error: e.message,
+      }),
+    }
   }
 }
