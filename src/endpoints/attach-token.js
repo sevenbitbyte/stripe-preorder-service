6342b352206f23d7b37e76a37b6e5249f6e84d26
clovis1122 <clovis-@live.com>
2019-11-24 12:05:21 -0400

    wraps attach token in try/catch

--- src/endpoints/attach-token.js
+++ src/endpoints/attach-token.js
@@ -9,43 +9,59 @@ const LookupAccount = require('../utils/lookup-account')
 
 const schema = Joi.object().keys({
   jwt: Joi.string().required(),
-  tokenId: Joi.string().required()
-});
+  tokenId: Joi.string().required(),
+})
 
 module.exports.attach_token = async (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false; 
+  context.callbackWaitsForEmptyEventLoop = false
 
   console.log('has account')
 
   console.log(event.body)
 
-  const valid = Joi.attempt(
-    JSON.parse(event.body),
-    schema
-  )
+  try {
+    const valid = Joi.attempt(JSON.parse(event.body), schema)
 
-  const accountInfo = await LookupAccount(valid.jwt)
+    const accountInfo = await LookupAccount(valid.jwt)
 
-  if(!accountInfo.customerId){  throw new Error('no stripe customer') }
-  if(!accountInfo.emailVerified){  throw new Error('not verified') }
+    if (!accountInfo.customerId) {
+      throw new Error('no stripe customer')
+    }
+    if (!accountInfo.emailVerified) {
+      throw new Error('not verified')
+    }
 
-  let card = null
-
-  debug('setting default source/token for customer', accountInfo.customerId, accountInfo.email, valid.tokenId)
-  card = await stripe.customers.update(
-    accountInfo.customerId,
-    {source: valid.tokenId}
-  )
-
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-    },
-    body: JSON.stringify({
-      tokenId: valid.tokenId
+    let card = null
+    debug(
+      'setting default source/token for customer',
+      accountInfo.customerId,
+      accountInfo.email,
+      valid.tokenId
+    )
+    card = await stripe.customers.update(accountInfo.customerId, {
+      source: valid.tokenId,
     })
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        tokenId: valid.tokenId,
+      }),
+    }
+  } catch (e) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+        error: e.message,
+      }),
+    }
   }
 }
